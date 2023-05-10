80ea92123cf9b69cce576231e2e3ae771a432b1a
DonutPoppa <georgetaylorkishi1269@gmail.com>
2023-05-10 15:50:39 +0900

    raise Exceptionを使うとプログラマーがエラーを決められます

--- error_shori.py
+++ error_shori.py
@@ -1,10 +1,14 @@
 #ValueErrorに対処する例外処理の例
 #try-except-else-finally文の使用
-try:
-    pin = int(input("PINを入力してください: "))
-except ValueError:
-    print("数値だけ入力してください")
-else: 
-    print("あなたの入力:", pin)
-finally:
-    print("もう終わりましたか？")
+#try:
+#    pin = int(input("pinを入力してください: "))
+#except valueerror:
+#    print("数値だけ入力してください")
+#else: 
+#    print("あなたの入力:", pin)
+#finally:
+#    print("もう終わりましたか？")
+character_name = "Tsubasa"
+character_name2 = "Carl"
+if character_name == "Tsubasa":
+    raise Exception("ははは")

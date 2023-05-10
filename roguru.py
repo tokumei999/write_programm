6d87236f003a68b2ee55b26e3d4047c022713c1a
DonutPoppa <georgetaylorkishi1269@gmail.com>
2023-05-10 16:06:47 +0900

    ランダム化されているかprint()でたしかめる

---
+++ roguru.py
@@ -0,0 +1,17 @@
+import random
+
+strength = 0
+brains = 0
+stamina = 0
+wisdom = 0
+constitutuion = 0
+dexterity = 0
+speed = 0
+
+strength = random.randint(1,20)
+brains = random.randint(1,20)
+stamina = random.randint(1,20)
+wisdom = random.randint(1,20)
+constitutuion = random.randint(1,20)
+dexterity = random.randint(1,20)
+speed = random.randint(1,20)

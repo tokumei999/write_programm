8345af6650b3f596c947af4d521d8b13c31ad01e
DonutPoppa <georgetaylorkishi1269@gmail.com>
2023-05-10 15:10:19 +0900

    checkingが行われている間はTrueで、最後の文でFalseにしてプログラムを終了している

--- error_shori.py
+++ error_shori.py
@@ -1,6 +1,11 @@
-try:
-    pin = int(input("PINを入力してください"))#整数のみ、入力を受け付けるint(input())
-    print("あなたの入力:", pin)
-except ValueError:
-    print("数値だけ入力してください")
+checking = True
+while checking:
+    #一行目と二行目で変数が実行されている間は以下の処理を行え
+    #そしてcheking = Falseでプログラムを終了。
+    try:
+        pin = int(input("PINを入力してください"))#整数のみ、入力を受け付けるint(input())
+        print("あなたの入力:", pin)
+        checking = False
+    except ValueError:
+        print("数値だけ入力してください")
 #pythonはexceptを実行するとプログラムを終了する。

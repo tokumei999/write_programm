a0bf5f6b76583a174e48117584336cafc8a43681
DonutPoppa <georgetaylorkishi1269@gmail.com>
2023-05-10 15:05:07 +0900

    except文が行われるとプログラムが終了する。ループさせることで繰り返し入力を受け付ける事が可能なので次はそうする

--- error_shori.py
+++ error_shori.py
@@ -1,4 +1,6 @@
-pin = int(input("PINを入力してください"))
-
-print("あなたの入力:", pin)
-
+try:
+    pin = int(input("PINを入力してください"))#整数のみ、入力を受け付けるint(input())
+    print("あなたの入力:", pin)
+except ValueError:
+    print("数値だけ入力してください")
+#pythonはexceptを実行するとプログラムを終了する。

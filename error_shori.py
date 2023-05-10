8de79e2288bf4faf4714989a4942c67089f18fb6
DonutPoppa <georgetaylorkishi1269@gmail.com>
2023-05-10 16:19:05 +0900

    あとはもう根性エラー処理です

--- error_shori.py
+++ error_shori.py
@@ -1,13 +1,10 @@
-#ValueErrorに対処する例外処理の例
-#try-except-else-finally文の使用
-#try:
-#    pin = int(input("pinを入力してください: "))
-#except valueerror:
-#    print("数値だけ入力してください")
-#else: 
-#    print("あなたの入力:", pin)
-#finally:
-#    print("もう終わりましたか？")
-#一行目はエラーがでない。assertの条件式がTrueになるから。二行目はFalseになりAssertionErrorを吐く
-assert 1 + 1 == 2
-assert 2 + 2 == 5
+ValueErrorに対処する例外処理の例
+try-except-else-finally文の使用
+try:
+    pin = int(input("pinを入力してください: "))
+except valueerror:
+    print("数値だけ入力してください")
+else: 
+    print("あなたの入力:", pin)
+#エラー処理のコツ:コメントアウトして一つ一つの書き直す。
+#原因がわかったらコメントアウトした行を削除する。

94ec2969c0965a9fff9e039e2615c0f3312a5451
DonutPoppa <georgetaylorkishi1269@gmail.com>
2023-05-10 18:03:01 +0900

    ファイル操作を使う明確な場面はRPAみたいな、自動化処理です。重要です。学びましょう。

--- file_by_python.py
+++ file_by_python.py
@@ -1,3 +1,4 @@
+#pythonでファイルの取り扱うときは恐らくはRPAみたいに、手順通りファイルを開かせて人がやる処理を自動化する場合に使えるのでは？twitter開いて、おはようツイートしてfacebookでも同じことするならその処理をするファイルを一つずつ用意してpythonファイルの操作をするパイソンファイルを作っておいて、実行すればいいからな。
 #ファイルを開く
 #同名ファイルが存在しているとそれを上書きしてしまうのです。
 new_file = open("createdfile.txt", "w")
@@ -7,8 +8,18 @@ new_file.write("Pythonのプログラムで新しいファイルを作成しま�
 new_file.write("二行目です\n")
 #close()関数でファイルをとじて保存する
 new_file.close()
+#ここで一つの作業が終わったと捉える
+
 #xモードを使うと既に同名ファイルがある場合失敗してエラーを出す。
 #作成したcreatedfile.txtを開く
 created_file = open("createdfile.txt", "r")
 # ファイル内容の読み込み
-print(created_file.read())
+#print(created_file.read())
+#同じファイルでやるのね。。。別のファイルでreadlineメソッドを使用していたらネームエラーでた。
+print(created_file.readline())
+#二行目を読み込み
+print(created_file.readline())
+#下のreadline「s」メソッドを使用するとファイル内のすべての行をリストとして読み込むよ。
+#print(created_file.readlines())
+#close()関数を忘れずに。
+new_file.close()

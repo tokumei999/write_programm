d7f24522d4a770b2bbb31c9e6592c1102282d84f
DonutPoppa <georgetaylorkishi1269@gmail.com>
2023-05-10 15:33:44 +0900

    最後に何があっても実行するfinallyを追加

--- error_shori.py
+++ error_shori.py
@@ -1,9 +1,10 @@
-checking = True
-while checking:
-    try:
-        pin = int(input("PINを入力してください: "))
-    except ValueError:
-        print("数値だけ入力してください")
-    else: 
-        print("あなたの入力:", pin)
-        checking = False
+#ValueErrorに対処する例外処理の例
+#try-except-else-finally文の使用
+try:
+    pin = int(input("PINを入力してください: "))
+except ValueError:
+    print("数値だけ入力してください")
+else: 
+    print("あなたの入力:", pin)
+finally:
+    print("もう終わりましたか？")

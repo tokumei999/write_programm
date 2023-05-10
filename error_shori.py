d55732463be56e5eb52ffe0d5e520903d60e3f3a
DonutPoppa <georgetaylorkishi1269@gmail.com>
2023-05-10 15:14:35 +0900

    try,except,else文を使って同じ内容のプログラムを作った。こちらのほうが読みやすい。

--- error_shori.py
+++ error_shori.py
@@ -1,11 +1,9 @@
 checking = True
 while checking:
-    #一行目と二行目で変数が実行されている間は以下の処理を行え
-    #そしてcheking = Falseでプログラムを終了。
     try:
-        pin = int(input("PINを入力してください"))#整数のみ、入力を受け付けるint(input())
-        print("あなたの入力:", pin)
-        checking = False
+        pin = int(input("PINを入力してください: "))
     except ValueError:
         print("数値だけ入力してください")
-#pythonはexceptを実行するとプログラムを終了する。
+    else: 
+        print("あなたの入力:", pin)
+        checking = False

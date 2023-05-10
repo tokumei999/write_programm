a291cdea13b46036710b77848eba7f9ed00867d4
DonutPoppa <georgetaylorkishi1269@gmail.com>
2023-05-10 16:10:47 +0900

    確認を完了したので次のコミットで消した事を記して書く

--- roguru.py
+++ roguru.py
@@ -9,9 +9,16 @@ dexterity = 0
 speed = 0
 
 strength = random.randint(1,20)
+print(strength)
 brains = random.randint(1,20)
+print(brains)
 stamina = random.randint(1,20)
+print(stamina)
 wisdom = random.randint(1,20)
-constitutuion = random.randint(1,20)
+print(wisdom)
+constitution = random.randint(1,20)
+print(constitution)
 dexterity = random.randint(1,20)
+print(dexterity)
 speed = random.randint(1,20)
+print(speed)
